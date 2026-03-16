b330c80d9dfe208b102d469dcb1df5b236a7dc59
kavinkumar1603 <kavin88701@gmail.com>
2026-03-16 22:03:06 +0530

    Add About, Services, WhyUs, Testimonials, and Blog components to Home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,19 @@
 import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
+import WhyUs from "./components/WhyUs";
+import Testimonials from "./components/Testimonials";
+import Blog from "./components/Blog";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
+      <Services />
+      <WhyUs />
+      <Testimonials />
+      <Blog />
     </main>
   );
 }
