e411434f6da79aea160d4a993a59f30984ceff22
kavinkumar1603 <kavin88701@gmail.com>
2026-03-16 19:44:18 +0530

    Refactor Services component and update styles for improved consistency

--- client/app/services/page.tsx
+++ client/app/services/page.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import Services from "../components/Services";
 
 const ServicesPage: React.FC = () => {
-  return (
-    <main className="pt-8">
-      <Services />
-    </main>
-  );
+  return <Services />;
 };
 
 export default ServicesPage;
